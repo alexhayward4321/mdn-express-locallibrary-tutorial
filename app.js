bd0693db3fe6487cccf657ed9a10da4d7d3c7da8
Alex Hayward <alexhayward4321@gmail.com>
2024-03-30 21:16:11 +0000

    First version of application moved to github

---
+++ app.js
@@ -0,0 +1,74 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(compression());
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			"script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+		},
+	})
+);
+
+const Limiter = RateLimit({
+	windowMs: 1 * 1000 * 60,
+	max: 20,
+});
+app.use(Limiter);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
+});
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const dev_db_url =
+	"mongodb+srv://alexhayward4321:A4TXhsAP3XoS8u8g@cluster0.k32eyh7.mongodb.net/locallibrary?retryWrites=true&w=majority&appName=Cluster0";
+
+const mongoDB = process.env.MONGO_DB_URI || dev_db_url;
+
+main().catch((err) => console.log(err));
+async function main() {
+	await mongoose.connect(mongoDB);
+}
+
+module.exports = app;
